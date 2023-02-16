d21be54015bf1ac54f81302e0f70d4d8cfe270c8
EvgenKlo <klokovevgen@gmail.com>
2023-02-16 23:38:25 +0500

    feat:done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let result = false;
+  let massiveStr = str.split('');
+  function findPair () {
+    for (let i = 0; i < massiveStr.length; i++){
+      bracketsConfig.forEach(element => {
+        if(massiveStr[i] === element[0] && massiveStr[i+1] === element[1]){
+          massiveStr.splice(i, 2);
+          findPair();
+        };
+        if(massiveStr.length === 0){
+          result = true;
+        };
+      });
+    }
+  };
+  findPair();
+  return result;
+};
